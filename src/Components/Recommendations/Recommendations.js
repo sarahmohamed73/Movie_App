dfd5ae0c8af946d51e09b232a7b70470da3b7a38
rehamtaher10 <rehamtaherelmshd@gmail.com>
2023-10-07 18:05:50 +0300

    Upload Recommendations

--- src/Components/Recommendations/Recommendations.js
+++ src/Components/Recommendations/Recommendations.js
@@ -6,16 +6,38 @@ import MovieCard from '../MoviesList/MovieCard';
 
 
 export default function Recommendations() {
-  
-  useEffect(() => {
-    recommends(775552)
-      .then((result) => console.log(result.data.results))
-      .catch((error) => console.log(error));
-    }, []);
+  const params = useParams()
+  const [recommendsMovie, setRecommendsMovie] = useState([])
+  const navigate = useNavigate()
 
+  useEffect(() => {
+    recommends(params.id)
+      .then((result) => setRecommendsMovie(result.data.results))
+      .catch((error) => console.log(error));
+    }, [params.id]);
+
+    const redirectToDetails = (id) => {
+      navigate(`/Details/${id}`)
+    }
   return (
     <div>
-      
+      <h2 className='text-start my-5'>Recommendations</h2>
+      {
+      recommendsMovie.length != 0 ? 
+        <div className="row row-cols-1 row-cols-lg-6 row-cols-md-2 g-4 justify-content-center align-items-center mb-5">
+          {recommendsMovie.map((movie, index) => {
+            while (index <= 5) {
+              return (
+                <div className="col d-flex justify-content-center align-items-center" key={movie.id}>
+                  <MovieCard movie={movie} navigateDetail={(id) => redirectToDetails(id)}/>
+                </div>
+              );
+            }
+          })}
+        </div>
+        :
+        <p className='mb-5 fw-medium fs-4 '>Sorry, I Havn't Any Recommends For You</p>
+      }
     </div>    
   )
 }
